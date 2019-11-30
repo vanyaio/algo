dc665c2bbc133246348232038f06c16d1d5d88b2
Vanya Io <differentstuff777@gmail.com>
2019-11-30 17:30:36 +0300

    code is written

--- p1.py
+++ p1.py
@@ -1,6 +1,9 @@
 import random
 import itertools
 import time
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def dfs(v, used, ans, edges):
     used[v] = True
@@ -52,30 +55,54 @@ def gen_by_ev(e_num, v_num):
             e_num -= 1
     return apply_perm(edges)
 
-def get_ev_by_sum(sum):
-    while True:
-        e = random.randint(0, sum - 1)
-        v = sum - e
-        if (e > (v - 1) * v / 2):
-            continue
-        else:
-            return (e, v)
+def get_ev_by_sum(sum, ratio):
+    #  while True:
+        #  e = random.randint(0, sum - 1)
+        #  v = sum - e
+        #  if (e > (v - 1) * v / 2):
+            #  continue
+        #  else:
+            #  return (e, v)
+    # To proof linear keep v/e the same during experiment
+    v = sum // ratio
+    e = sum - v
+    if (e > (v - 1) * v / 2):
+        print("ratio is incorrect!!!")
+    return (e, v)
+
+def draw(es, vs, ts):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(np.array(es), np.array(vs), np.array(ts))
+    ax.set_xlabel("E")
+    ax.set_ylabel("V")
+    ax.set_zlabel("time(sec)")
+    ax.set_title("Time from Edges and Vertices")
+    plt.show()
 
 def main():
-    sum = 100000
-    steps = 31
-    delta = 5000
+    sum = 10000
+    steps = 15
+    delta = 1000
+    ratio = 5 
+    es = []
+    vs = []
+    ts = []
     for i in range(steps):
-        e, v = get_ev_by_sum(sum)
+        e, v = get_ev_by_sum(sum, ratio)
         edges = gen_by_ev(e, v)
         
         start = time.time()
         topsort(edges)
         end = time.time()
-        print(end - start)
 
-        print(sum)
+        es.append(e)
+        vs.append(v)
+        ts.append(end - start)
+
         sum += delta
+
+    draw(es, vs, ts)
 
 
 if __name__ == "__main__":
